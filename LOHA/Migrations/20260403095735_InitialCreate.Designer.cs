cc261df1071fdd0b781bd984c430e362aac2e2d2
Bui Duc Ha <phucducha12@gmail.com>
2026-04-04 14:42:32 +0700

    cập nhật icon

--- LOHA/Migrations/20260403095735_InitialCreate.Designer.cs
+++ LOHA/Migrations/20260403095735_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LOHA.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260403085913_InitialCreate")]
+    [Migration("20260403095735_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
