9111af53fadfdb576a72b9813f2eb1b69b7cc643
Minhtran-17 <dtc245270131@ictu.edu.vn>
2026-04-11 16:50:18 +0700

    res

---
+++ LOHA/Migrations/20260411095722_capnhatuser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LOHA.Migrations
+{
+    /// <inheritdoc />
+    public partial class capnhatuser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AnhNen",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AnhNen",
+                table: "Users");
+        }
+    }
+}
